f9043250a3796a86d23e91cbd5b8874b3ae6509f
simonshephard <simonshephard@hotmail.com>
2018-12-31 18:45:13 +0000

    adds logger

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,11 +1,15 @@
 package com.mycompany.app;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  */
 public class App
 {
 
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
     private static final String MESSAGE = "Hello World!";
 
     public App() {
@@ -13,7 +17,8 @@ public class App
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+        // System.out.println(new App().getMessage());
+        LOGGER.log(Level.SEVERE, new App().getMessage());
     }
 
     private final String getMessage() {
